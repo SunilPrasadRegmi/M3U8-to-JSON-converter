eb458ba1641d28ed26bbb3214ceaeeafd5ee5245
SunilPrasadRegmi <90543452+SunilPrasadRegmi@users.noreply.github.com>
2023-04-15 12:18:39 +0545

    Update script.py
    fixed for some playlists are not supported..

--- script.py
+++ script.py
@@ -8,10 +8,15 @@ channels = []
 
 for line in content:
     if '#EXTINF' in line:
-        tvg_id = re.search('tvg-id="(.*?)"', line).group(1)
-        tvg_chno = re.search('tvg-chno="(.*?)"', line).group(1)
-        tvg_logo = re.search('tvg-logo="(.*?)"', line).group(1)
-        group_title = re.search('group-title="(.*?)"', line).group(1)
+        tvg_id_match = re.search('tvg-id="(.*?)"', line)
+        tvg_chno_match = re.search('tvg-chno="(.*?)"', line)
+        tvg_logo_match = re.search('tvg-logo="(.*?)"', line)
+        group_title_match = re.search('group-title="(.*?)"', line)
+        
+        tvg_id = tvg_id_match.group(1) if tvg_id_match else None
+        tvg_chno = tvg_chno_match.group(1) if tvg_chno_match else None
+        tvg_logo = tvg_logo_match.group(1) if tvg_logo_match else None
+        group_title = group_title_match.group(1) if group_title_match else None
         channel_name = line.split(',')[1].strip()
         url = content[content.index(line) + 1].strip()
 
